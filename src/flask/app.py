e569e8a5700b8a46ae09818a170bdad74d1127b8
Jesse <jesse1517@gmail.com>
2020-04-07 11:56:14 -0700

    it reads the file :')

--- src/flask/app.py
+++ src/flask/app.py
@@ -18,22 +18,35 @@ def info_route():
 
 @app.route("/close")
 def error_route():
-    #closing database connection.
-    if(connection):
-        cursor.close()
-        connection.close()
-        return "PostgreSQL connection is closed"
+    db1 = DB_info()
+    data_folder = Path('/var/snap/flasksnap/common/config/')
+    file_to_open = data_folder / "text-file.txt"
+    info = None
+    with open(str(file_to_open), 'r') as f:
+        info = [line.split() for line in f]
+
+    for line in info:
+        if line[0] == 'DB_USER':
+            db1.set_user(line[2][1:-1])
+        elif line[0] == 'DB_PASSWORD':
+            db1.set_password(line[2][1:-1])
+        elif line[0] == 'DB_HOST':
+            db1.set_host(line[2][1:-1])
+        elif line[0] == 'DB_PORT':
+            db1.set_port(line[2][1:-1])
+        elif line[0] == 'DB_NAME':
+            db1.set_dbname(line[2][1:-1])
+
+    a = str(db1.get_user() + " " +  db1.get_password() + " " +  db1.get_host() + " " +  db1.get_port())
+    return a
 
 @app.route("/test")
 def tester():
     db1 = DB_info()
     data_folder = Path('/var/snap/flasksnap/common/config/')
     file_to_open = data_folder / "text-file.txt"
-
     info = None
-
-
-    with open(str(file_to_open), 'r') as f:
+    with open(str(file_to_open), 'r', encoding="utf-8") as f:
         info = [line.split() for line in f]
 
     for line in info:
@@ -52,30 +65,13 @@ def tester():
     return a
 
 
-@app.route("/other")
-def other():
-    _db1 = DB_info()
-
-    _file_to_open = _data_folder / "thecodes.txt"
-
-    info = None
-
-
-    with open(_file_to_open, 'r', encoding="utf-8") as f:
-        info = [line.encode('utf-8').strip("'") for line in f]
-    return info[0]
-
-
 if __name__ == '__main__':
 
     db = DB_info()
     data_folder = Path('/var/snap/flasksnap/common/config/')
     file_to_open = data_folder / "thecodes.txt"
-
     info = None
-
-
-    with open(str(file_to_open), 'r') as f:
+    with open(str(file_to_open), 'r', encoding="utf-8") as f:
         info = [line.split() for line in f]
 
         for line in info:
@@ -89,7 +85,6 @@ if __name__ == '__main__':
                 db.set_port(line[1][1:-1])
             elif line[0] == 'DB_NAME':
                 db.set_dbname(line[1][1:-1])
-    print("test1")
 
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
 
